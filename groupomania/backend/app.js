139b78600818fc21d79f6edff199b4c3177a157a
Morgane Diquelou <diquelou.morgane@hotmail.fr>
2022-03-19 15:59:52 +0100

    Packages added + server port config added + config

--- groupomania/backend/app.js
+++ groupomania/backend/app.js
@@ -1,8 +1,40 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête à bien été reçue !'});
-})
+//Roads import//
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
+const commentRoutes = require('./routes/comment');
+
+//CORS to authorize actions from the port 3000//
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    if (req.method === 'OPTIONS') {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+  });
+
+//Parse application/json content-type requests//
+app.use(bodyParser.json());
+
+//Call models in database//
+const db = require("./models");
+db.sequelize.sync();
+
+//Save routers//
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
 
 module.exports = app;
